fa92beac998f8c667d4807d1915015a34f2cea2d
tsegaye27 <ttalegn29@gmail.com>
2024-06-25 13:29:34 +0300

    made some changes

---
+++ pages/auth/signup.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex w-[100%] h-[100vh] justify-center items-center">
+    <div class="left w-[38.19%] h-[100%]">
+      <h1
+        class="fixed font-bold text-[82px] text-white text-center mt-[4.5rem] w-[38.19%] h-[96px] z-0"
+      >
+        Afalagi
+      </h1>
+      <p
+        class="fixed font-[poppins] text-center font-semibold text-[36px] text-white mt-[13rem] w-[478px] h-[130px] z-1"
+      >
+        Connecting Hearts, Finding Hope
+      </p>
+      <img
+        class="object-fit w-[500px] h-full"
+        src="/assets/img/signup.png"
+        alt="two-people-hugging"
+      />
+    </div>
+    <div
+      class="right w-[61.81%] h-[680px] flex justify-between mt-[68px] items-center flex-col"
+    >
+      <h1 class="text-center font-semibold text-[82px] text-[#676767]">Signup</h1>
+      <FormCard />
+    </div>
+  </div>
+</template>
+
+<script setup></script>
