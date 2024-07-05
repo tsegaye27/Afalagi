0f58452e6d31201ceca2ad70dfd40639cf2903a4
John Assefa <johnassefaforbussines@gmail.com>
2024-07-05 01:44:35 -0700

    adjuste the search box

--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="search h-[40px]">
+  <div class="search h-[40px] flex">
     <input
       type="text"
       class="h-[40px] rounded-[18px_0_0_18px] pl-4 border-2 border-[#788dd5] shadow-[1px_1px_2px_1px_lightgray] text-[#868686] outline-none"
