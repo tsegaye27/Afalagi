aec785e26b029ff21c67ab85d08b28154fc647c7
tsegaye27 <ttalegn29@gmail.com>
2024-07-12 23:22:36 +0300

    made some changes

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,9 +1,18 @@
 <template>
   <div
-    class="w-[280px] h-[655px] flex flex-col mt-1 justify-between items-center shadow-md bg-[#f4f4f4]"
+    class="w-[280px] h-[655px] flex flex-col my-1 justify-center gap-[1rem] items-center shadow-md bg-[#f4f4f4]"
   >
-    <div class="w-[80px] mt-[5rem] h-[80px] bg-slate-500 rounded-full" />
-    <ul class="pt-[5rem] flex flex-col h-[50%] gap-[0.5rem] w-[100%]">
+    <div
+      class="w-[200px] h-[200px] flex flex-col gap-[2rem] justify-center items-center"
+    >
+      <img
+        class="w-[80px] h-[80px] rounded-[2.5rem]"
+        src="@/assets/img/missing.png"
+        alt="profile-picture"
+      />
+      <p class="text-xl font-semibold">John Doe</p>
+    </div>
+    <ul class="pt-[2rem] flex flex-col h-[50%] gap-[0.5rem] w-[100%]">
       <nuxt-link class="py-1 w-[100%]" to="/profile">
         <li
           class="border border-slate-300 w-[100%] text-center rounded-md cursor-pointer hover:text-[#788dd5]"
@@ -33,6 +42,11 @@
         </li>
       </nuxt-link>
     </ul>
-    <button class="w-[100%] mb-[30px] text-red-600">Logout</button>
+    <button
+      @click="navigateTo('/auth/login')"
+      class="w-[100%] mb-[30px] text-red-600"
+    >
+      Logout
+    </button>
   </div>
 </template>
