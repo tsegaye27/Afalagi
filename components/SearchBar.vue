e6660896259736365545ed8c4933b8371fcbd7d6
tsegaye27 <ttalegn29@gmail.com>
2024-07-12 23:22:25 +0300

    made some changes

--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -1,12 +1,20 @@
+<script setup>
+const input = ref("John");
+const handleSearch = () => {
+  // searching takes place here
+};
+</script>
 <template>
   <div class="search h-[40px] flex">
     <input
       type="text"
-      class="h-[40px] rounded-[18px_0_0_18px] pl-4 border-2 border-[#788dd5] shadow-[1px_1px_2px_1px_lightgray] text-[#868686] outline-none"
+      class="h-[41px] rounded-[18px_0_0_18px] pl-4 border-2 border-[#788dd5] text-[#868686] outline-none"
       placeholder="Start typing here..."
+      v-model="input"
     />
     <button
       class="p-2 h-[41px] px-5 mr-3 bg-[#788DD5] rounded-[0_18px_18px_0] text-white"
+      @click="handleSearch"
     >
       ->
     </button>
