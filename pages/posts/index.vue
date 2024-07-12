d15d11a57d6c305a7c21bc3a187f784d2d648f39
tsegaye27 <ttalegn29@gmail.com>
2024-07-12 23:22:48 +0300

    made some changes

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,7 +1,47 @@
+<script setup>
+const missingPersons = [
+  {
+    fullName: "John Charles Doe",
+    age: 30,
+    gender: "Male",
+    location: "New York, USA",
+    lastSeenWearing: "T-shirt and jeans",
+    dateMissing: "2024-07-01",
+    description:
+      "John was last seen near Central Park at around 5 PM on July 1st, 2024. He is 6 feet tall with brown hair and blue eyes. John has a distinctive scar on his left cheek and a tattoo of a dragon on his right arm. He was carrying a black backpack.",
+    imgURL: "",
+    videoMessage: "",
+  },
+  {
+    fullName: "Jane Smith",
+    age: 25,
+    gender: "Female",
+    location: "London, UK",
+    lastSeenWearing: "Summer dress",
+    dateMissing: "2024-07-05",
+    description:
+      "Jane was last seen in Hyde Park around 3 PM on July 5th, 2024. She is 5 feet 5 inches tall with blonde hair and green eyes. Jane was wearing a light blue summer dress and white sandals. She has a small birthmark on her right wrist and was carrying a red handbag.",
+    imgURL: "",
+    videoMessage: "",
+  },
+  {
+    fullName: "Alex Johnson",
+    age: 28,
+    gender: "Male",
+    location: "Toronto, Canada",
+    lastSeenWearing: "Hoodie and shorts",
+    dateMissing: "2024-07-07",
+    description:
+      "Alex was last seen near Queen Street at around 8 PM on July 7th, 2024. They are 5 feet 8 inches tall with black hair and brown eyes. Alex was wearing a grey hoodie, black shorts, and white sneakers. They have a nose piercing and a sleeve tattoo on their left arm. Alex was last seen with a friend who reported them missing.",
+    imgURL: "",
+    videoMessage: "",
+  },
+];
+</script>
 <template>
   <div class="mt-[3rem]">
     <div
-      class="flex mx-[2.5rem] mb-[2rem] gap-[14rem] justify-start items-center"
+      class="flex mx-[2.5rem] mb-[2rem] gap-[5rem] justify-start items-center"
     >
       <h1 class="text-[40px] text-[#868686] font-[poppins] font-semibold">
         Missing Reported
@@ -13,16 +53,18 @@
     <div
       class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-[2rem] m-[2rem]"
     >
-      <MissingCard />
-      <MissingCard />
-      <MissingCard />
-      <MissingCard />
-      <MissingCard />
-      <MissingCard />
-      <MissingCard />
-      <MissingCard />
+      <MissingCard
+        v-for="person in missingPersons"
+        :key="person.fullName"
+        :name="person.fullName"
+        :location="person.location"
+        :date="person.dateMissing"
+        :cloth="person.lastSeenWearing"
+        :description="person.description"
+        :videoMessage="person.videoMessage"
+        :gender="person.gender"
+        :age="person.age"
+      />
     </div>
   </div>
 </template>
-
-<script setup></script>
