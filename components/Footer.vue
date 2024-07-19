a6f616cf3bdb3851af8d222b95dc71449d75907f
tsegaye27 <ttalegn29@gmail.com>
2024-07-20 02:52:24 +0300

    made some changes

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,12 +2,16 @@
   <div
     class="footer flex justify-start items-center w-[100%] h-[200px] bg-[#b8BDBDBD]"
   >
-    <div
-      class="logo text-[3rem] pl-[3rem] pt-[1.5rem] h-[100%] text-[#788DD5] font-[poppins] flex flex-col justify-start font-semibold"
-    >
-      <nuxt-link to="/"> Afalagi </nuxt-link>
+    <div>
+      <nuxt-link to="/">
+        <img
+          class="logo w-[200px] h-[130px] object-contain ml-[5rem]"
+          src="@/assets/img/logo.png"
+          alt="reuniteus_logo"
+        />
+      </nuxt-link>
     </div>
-    <ul class="mx-[8rem] flex flex-col items-start">
+    <ul class="mr-[6rem] flex flex-col items-start">
       <li class="nav-menu"><NuxtLink to="/">Home</NuxtLink></li>
       <li class="nav-menu"><NuxtLink to="/posts">Posts</NuxtLink></li>
       <li class="nav-menu">
@@ -15,8 +19,8 @@
       </li>
     </ul>
     <ul class="mx-[13rem] flex flex-col items-start pt-[3rem]">
-      <li class="text-[#868686] text-[18px]">info@afalagi.com</li>
-      <li class="text-[#868686] text-[18px]">+251 912345678</li>
+      <li class="text-[#005782] text-[18px]">info@afalagi.com</li>
+      <li class="text-[#005782] text-[18px]">+251 912345678</li>
     </ul>
     <div class="mx-[5rem] pt-[4.5rem]">
       <div class="flex justify-between pb-2">
@@ -24,8 +28,8 @@
         <img class="w-[35px] h-[30px]" src="/assets/img/youtube.svg" />
         <img class="w-[35px] h-[30px]" src="/assets/img/tiktok.png" />
       </div>
-      <p class="text-[#868686]">Afalagi All Rights Reserved</p>
-      <p class="text-[#868686]">&copy; Copyright 2024</p>
+      <p class="text-[#005782]">Afalagi All Rights Reserved</p>
+      <p class="text-[#005782]">&copy; Copyright 2024</p>
     </div>
   </div>
 </template>
