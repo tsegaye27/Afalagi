97e12472637892277bd2933afcf79ba33818f485
John Assefa <johnassefaforbussines@gmail.com>
2024-07-23 01:51:34 -0700

    editing layout

--- stores/user.ts
+++ stores/user.ts
@@ -6,48 +6,42 @@ import {defineStore} from 'pinia'
 export const useUserStore=defineStore('user',()=>{
     const user=ref();
     const token =useCookie('afalagis_cookie',{
+        maxAge:60*9
+    })
+    const refreshToken=useCookie('afalagis_refresh_cookie',{
         maxAge:60*60*24*7
     })
 
     const setToken=(data?:string)=>(token.value=data)
     const setUser=(data?:any)=>(user.value=data)
+    const setRefreshToken=(data?:string)=>(refreshToken.value=data)
 
 
-    const signIn=async(data:any)=>{
-        try{
-            const res=await axios.post('https://dummyjson.com/auth/login', {
-                Headers: {
-                  'Content-Type': 'application/json'  
-                },
-                Body:data
-              });
+    // const signIn=async(data:any)=>{
+    //     try{
+    //         const res=await axios.post('https://dummyjson.com/auth/login', {
+    //             Headers: {
+    //               'Content-Type': 'application/json'  
+    //             },
+    //             Body:data
+    //           });
 
-              setToken(res.data.token);
-              await fetchUser();
-        }
-        catch(error){
-            setToken();
-            setUser();
-            console.error(error); 
-        }
-    }
+    //           setToken(res.data.token);
+    //           await fetchUser();
+    //     }
+    //     catch(error){
+    //         setToken();
+    //         setUser();
+    //         console.error(error); 
+    //     }
+    // }
     const fetchUser=async()=>{
-        if(token.value){
-            try{
-                const res=await axios.post('https://dummyjson.com/users/1');
-                setUser(res.data)
-            }catch(error){
-                setToken();
-                setUser();
-                console.error(error); 
-
-            }
-        }
+        
     }
     const logout=()=>{
         setToken();
         setUser();
     }
-
-    return{ user,token,logout,signIn,fetchUser}
+// signIn
+    return{ user,token,logout,setRefreshToken,fetchUser}
 })
