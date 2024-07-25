ad8ce616bddea15f8d5418e77f584bbf8087053e
John Assefa <johnassefaforbussines@gmail.com>
2024-07-25 00:05:07 -0700

    adjusting the store

--- stores/user.ts
+++ stores/user.ts
@@ -1,40 +1,15 @@
-import { Body } from '#build/components';
-import axios from 'axios';
 import {defineStore} from 'pinia'
 
 
 export const useUserStore=defineStore('user',()=>{
     const user=ref();
-    const token =useCookie('afalagis_cookie',{
-        maxAge:60*9
-    })
-    const refreshToken=useCookie('afalagis_refresh_cookie',{
-        maxAge:60*60*24*7
-    })
+    const token =ref();
+    const refreshToken=ref();
 
     const setToken=(data?:string)=>(token.value=data)
     const setUser=(data?:any)=>(user.value=data)
     const setRefreshToken=(data?:string)=>(refreshToken.value=data)
 
-
-    // const signIn=async(data:any)=>{
-    //     try{
-    //         const res=await axios.post('https://dummyjson.com/auth/login', {
-    //             Headers: {
-    //               'Content-Type': 'application/json'  
-    //             },
-    //             Body:data
-    //           });
-
-    //           setToken(res.data.token);
-    //           await fetchUser();
-    //     }
-    //     catch(error){
-    //         setToken();
-    //         setUser();
-    //         console.error(error); 
-    //     }
-    // }
     const fetchUser=async()=>{
         
     }
