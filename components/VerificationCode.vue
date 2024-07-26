e9c730bec24f975da9749096f7425179f77543d1
tsegaye27 <ttalegn29@gmail.com>
2024-07-27 00:41:54 +0300

    integrating the verification page

--- components/VerificationCode.vue
+++ components/VerificationCode.vue
@@ -1,54 +1,54 @@
 <script setup>
-const verificationCode = ref([]);
+import { useUserStore } from "@/stores/user";
+
+const store = useUserStore();
+
+console.log(store);
+
+const inputs = ref(["", "", "", "", "", ""]);
 const verify = () => {
-  if (verificationCode.value) {
-    navigateTo("/");
-  }
+  const verificationCode = inputs.value.join("");
 };
 </script>
 <template>
   <div class="flex w-[100%] justify-center gap-[5rem] items-center">
     <input
+      v-for="(input, index) in inputs"
+      v-model="inputs[index]"
       type="text"
-      :id="1"
       :maxlength="1"
-      :value="0"
+      class="w-[50px] h-[50px] text-center text-[var(--primary-color)] text-[1.75rem] border-2 border-[var(--secondary-color)] outline-none rounded-lg"
+    />
+    <!-- <input
+    type="text"
+      :maxlength="1"
       class="w-[50px] h-[50px] text-center text-[var(--primary-color)] text-[1.75rem] border-2 border-[var(--secondary-color)] outline-none rounded-lg"
     />
     <input
       type="text"
-      :id="2"
       :maxlength="1"
-      :value="1"
       class="w-[50px] h-[50px] text-center text-[var(--primary-color)] text-[1.75rem] border-2 border-[var(--secondary-color)] outline-none rounded-lg"
     />
     <input
       type="text"
-      :id="3"
       :maxlength="1"
-      :value="2"
       class="w-[50px] h-[50px] text-center text-[var(--primary-color)] text-[1.75rem] border-2 border-[var(--secondary-color)] outline-none rounded-lg"
     />
     <input
       type="text"
-      :id="4"
       :maxlength="1"
-      :value="2"
       class="w-[50px] h-[50px] text-center text-[var(--primary-color)] text-[1.75rem] border-2 border-[var(--secondary-color)] outline-none rounded-lg"
     />
     <input
       type="text"
-      :id="5"
       :maxlength="1"
-      :value="2"
       class="w-[50px] h-[50px] text-center text-[var(--primary-color)] text-[1.75rem] border-2 border-[var(--secondary-color)] outline-none rounded-lg"
-    />
-    <input
-      type="text"
-      :id="4"
-      @input="verify"
-      :value="verificationCode.value"
-      class="w-[50px] h-[50px] text-center text-[var(--primary-color)] text-[1.75rem] border-2 border-[var(--secondary-color)] outline-none rounded-lg"
-    />
+    /> -->
   </div>
+  <button
+    class="p-3 rounded-md text-white bg-[var(--secondary-color)]"
+    @click="verify"
+  >
+    Verify
+  </button>
 </template>
--- pages/auth/verification.vue
+++ pages/auth/verification.vue
@@ -4,7 +4,7 @@ definePageMeta({ layout: "" });
 <template>
   <div class="h-[100vh] w-[100%] flex justify-center bg-[#f3f3f3] items-center">
     <div
-      class="bg-white rounded-md shadow-md h-[45%] px-[2rem] py-[1rem] flex flex-col justify-center gap-[3rem] items-center"
+      class="bg-white rounded-md shadow-md h-[65%] px-[2rem] py-[1rem] flex flex-col justify-center gap-[3rem] items-center"
     >
       <h1
         class="text-[var(--primary-color)] font-medium w-[70%] text-center text-[25px] uppercase"
