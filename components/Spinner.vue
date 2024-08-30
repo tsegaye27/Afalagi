7237f8ced47cd32531724d04da3a5f5cf4e88248
tsegaye27 <ttalegn29@gmail.com>
2024-08-30 21:38:06 +0300

    refactor: Update Spinner component with new styling and animation

--- components/Spinner.vue
+++ components/Spinner.vue
@@ -2,8 +2,26 @@
   <div
     class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"
   >
-    <div
-      class="animate-spin rounded-full h-12 w-12 border-t-4 border-blue-500"
-    ></div>
+    <div class="spinner"></div>
   </div>
 </template>
+
+<style>
+.spinner {
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  border: 4px solid #ffffff;
+  border-top-color: transparent;
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
