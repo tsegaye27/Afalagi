4ad6b9e2a7fd9163c3226ed7c81fb27b57ed960d
tsegaye27 <ttalegn29@gmail.com>
2024-09-09 22:10:05 +0300

    refactor: Improve responsive design of Hero and NavBar components

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <NavBar class="shadow-md px-[2rem] h-[6rem]" />
+    <NavBar class="shadow-md px-[2rem] h-[6rem] md:px-[1rem] md:h-[5rem]" />
     <div class="overflow-hidden">
       <slot />
       <Footer />
