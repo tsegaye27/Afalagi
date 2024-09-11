5b14ff3072c3e31e485589622c84e2122d390656
tsegaye27 <ttalegn29@gmail.com>
2024-09-11 19:28:26 +0300

    refactor: Disable devtools in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   css: ["~/assets/css/main.css", "~/assets/css/fonts.css"],
   postcss: {
     plugins: {
